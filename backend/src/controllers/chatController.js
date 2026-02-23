1320bb521c071ce4bbd6db3f184cc3624ab1e5b1
suppabhinandan-lab <supp.abhinandan@gmail.com>
2026-02-23 19:53:58 +0530

    Resolved the backend errors and added checks for the input given

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -4,9 +4,19 @@ import { generateAnswer } from "../services/chatService.js";
 export async function chat(req, res) {
   try{
 const { message } = req.body;
-  console.log("Received message:", message); 
-  const context = await searchText(message);
-  const answer = await generateAnswer(message, context);
+if(!message || typeof message==='string'){
+  return res.status(400).json({answer:"Invalid message"});
+}
+const cleanedMessage=message.trim().toLowerCase();
+  console.log("Received message:", cleanedMessage); 
+   const greetings = ["hello", "hi", "hey", "namaste"];
+    if (greetings.includes(cleanedMessage)) {
+      return res.json({
+        answer: "नमस्ते 🙏 मैं आपकी कैसे सहायता कर सकता हूँ?"
+      });
+    }
+  const context = await searchText(cleanedMessage);
+  const answer = await generateAnswer(cleanedMessage, context);
   console.log("Generated answer:", answer); 
   res.json({ answer });
 } 
