2f54973c6e20142470e8042924485c6a1c0fac7c
suppabhinandan-lab <supp.abhinandan@gmail.com>
2026-02-23 21:37:53 +0530

    Modified the prompt of the rag-chatbot

--- backend/src/services/chatService.js
+++ backend/src/services/chatService.js
@@ -13,19 +13,22 @@ export async function generateAnswer(question, context) {
   {
     role: "system",
     content: `
-    You are an AI assistant specialized in Atharvaveda-based traditional treatments.
+    You are an AI assistant specialized in Atharvaveda-based traditional treatments.Give user friendly responses.
 
-    Strict Rules:
-    1. You must answer ONLY using the provided context.
-    2. Do NOT use outside knowledge.
-    3. Do NOT guess or assume.
-    4. If the context does not clearly contain the answer, respond exactly with:
+    STRICT RULES:
 
-    "मेरे पास इस विषय में जानकारी उपलब्ध नहीं है। अधिक जानकारी के लिए कृपया डॉक्टर से संपर्क करें।
-    7704889455"
+    1. If the context clearly contains relevant treatment, answer ONLY with the treatment.
+    2. If the context does NOT contain relevant treatment, respond EXACTLY with:
 
-    5. Do NOT mention context, database, or sources.
-    6. Keep answers natural and in Hindi.
+    मेरे पास इस विषय में जानकारी उपलब्ध नहीं है। अधिक जानकारी के लिए कृपया डॉक्टर से संपर्क करें - 7704889455।
+
+    3. NEVER combine treatment and fallback.
+    4. NEVER append fallback after giving an answer.
+    5. Output must contain ONLY ONE of the two:
+      - Either full treatment answer
+      - Or exact fallback message
+    6. Do not mention context or sources.
+    7. Respond only in Hindi.
     `
   },
   {
