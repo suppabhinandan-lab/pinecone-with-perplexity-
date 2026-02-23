8f3ee22ecb26619298eceb27d1e0ce2a7e7fd18b
suppabhinandan-lab <supp.abhinandan@gmail.com>
2026-02-23 20:30:56 +0530

    Modified the rag-chatbot prompt

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -4,10 +4,19 @@ import { generateAnswer } from "../services/chatService.js";
 export async function chat(req, res) {
   try{
 const { message } = req.body;
-  console.log("Received message:", message); 
-  const context = await searchText(message);
-  const answer = await generateAnswer(message, context);
-  console.log("Generated answer:", answer); 
+if(!message){
+  return res.status(400).json({answer:"Error in getting message"});
+}
+    const cleanedMessage = message.trim().toLowerCase();
+    const greetings = ["hello", "hi", "hey", "namaste"];
+
+    if (greetings.includes(cleanedMessage)) {
+      return res.json({
+        answer: "नमस्ते 🙏 मैं आपकी कैसे सहायता कर सकता हूँ?"
+      });
+    }
+  const context = await searchText(cleanedMessage);
+  const answer = await generateAnswer(cleanedMessage, context);
   res.json({ answer });
 } 
 catch(error){
